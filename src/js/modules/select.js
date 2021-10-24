42878a51f1de914dc24b915b53b6323dd8c52110
Yury Uvarov <uvarov.frontend@gmail.com>
2021-10-25 01:59:09 +0300

    add search options

--- src/js/modules/select.js
+++ src/js/modules/select.js
@@ -11,23 +11,46 @@ class Select {
 					default: option?.select?.class?.select?.default ? option.select.class.select.default : 'vanilla-select',
 					show: option?.select?.class?.select?.show ? option.select.class.select.show : 'vanilla-select_show',
 					multiple: option?.select?.class?.select?.multiple ? option.select.class.select.multiple : 'vanilla-select_multiple',
+					single: option?.select?.class?.select?.single ? option.select.class.select.single : 'vanilla-select_single',
 				},
 				btn: option?.select?.class?.btn ? option.select.class.btn : 'vanilla-select__btn',
-				wrapper: option?.select?.class?.wrapper ? option.select.class.wrapper : 'vanilla-select__wrapper',
+				popup: option?.select?.class?.popup ? option.select.class.popup : 'vanilla-select__popup',
+				content: option?.select?.class?.content ? option.select.class.content : 'vanilla-select__content',
+				input: {
+					default: option?.select?.class?.input?.default ? option.select.class.input.default : 'vanilla-select__input',
+					active: option?.select?.class?.input?.active ? option.select.class.input.active : 'vanilla-select__input_active',
+				},
+				group: {
+					default: option?.select?.class?.group ? option.select.class.group : 'vanilla-select__group',
+					active: option?.select?.class?.group ? option.select.class.group : 'vanilla-select__group_active',
+				},
 				label: option?.select?.class?.label ? option.select.class.label : 'vanilla-select__label',
-				group: option?.select?.class?.group ? option.select.class.group : 'vanilla-select__group',
 				list: option?.select?.class?.list ? option.select.class.list : 'vanilla-select__list',
 				option: {
 					default: option?.select?.class?.option?.default ? option.select.class.option.default : 'vanilla-select__option',
 					selected: option?.select?.class?.option?.selected ? option.select.class.option.selected : 'vanilla-select__option_selected',
+					active: option?.select?.class?.option?.active ? option.select.class.option.active : 'vanilla-select__option_active',
 					disabled: option?.select?.class?.option?.disabled ? option.select.class.option.disabled : 'vanilla-select__option_disabled',
 					hidden: option?.select?.class?.option?.hidden ? option.select.class.option.hidden : 'vanilla-select__option_hidden',
 				},
+				plug: {
+					default: option?.select?.class?.plug.default ? option.select.class.plug.default : 'vanilla-select__plug',
+					active: option?.select?.class?.plug.active ? option.select.class.plug.active : 'vanilla-select__plug_active',
+				},
+			},
+			multiple: {
+				text: option?.select?.multiple?.text ? option.select.multiple.text : 'Выбрано:',
+			},
+			search: {
+				input: {
+					type: option?.select?.search?.input?.type ? option.select.search.input.type : 'text',
+					placeholder: option?.select?.search?.input?.placeholder ? option.select.search.input.placeholder : 'Поиск...',
+				},
+				plug: {
+					text: option?.select?.search?.plug?.text ? option.select.search.plug.text : 'Нет совпадений...',
+				},
 			},
 			show: false,
-			multiple: {
-				text: option?.select?.multiple?.text ? option?.select?.multiple?.text : 'Выбрано:',
-			},
 		};
 	}
 
@@ -41,31 +64,31 @@ class Select {
 		this.select.show = false;
 	}
 
-	selectedMultipleOption(option, btn, selectArr, selectDefault, selectDefaultActive) {
-		const lastOptionSelected = selectDefault.options[selectDefault.selectedIndex];
+	selectedMultipleOption(option, btn, selectArr, defaultSelect, defaultSelectActive) {
+		const lastOptionSelected = defaultSelect.options[defaultSelect.selectedIndex];
 
 		if (lastOptionSelected && lastOptionSelected.hasAttribute('hidden')) lastOptionSelected.removeAttribute('selected');
 		if (option.classList.contains(this.select.class.option.selected)) {
 			option.classList.remove(this.select.class.option.selected);
-			selectDefaultActive.removeAttribute('selected');
+			defaultSelectActive.removeAttribute('selected');
 		} else {
 			option.classList.add(this.select.class.option.selected);
-			selectDefaultActive.setAttribute('selected', 'selected');
+			defaultSelectActive.setAttribute('selected', 'selected');
 		}
 
-		const selectDefaultActiveOptions = selectArr.filter((defaultOption) => defaultOption.hasAttribute('selected'));
-		btn.innerText = `${this.select.multiple.text} ${selectDefaultActiveOptions.length}`;
+		const defaultSelectActiveOptions = selectArr.filter((defaultOption) => defaultOption.hasAttribute('selected'));
+		btn.innerText = `${this.select.multiple.text} ${defaultSelectActiveOptions.length}`;
 	}
 
-	selectedSingleOption(option, btn, parent, selectDefault, selectDefaultActive) {
+	selectedSingleOption(option, btn, parent, defaultSelect, defaultSelectActive) {
 		const optionActive = parent.querySelector(`.${this.select.class.option.default}.${this.select.class.option.selected}`);
 
 		optionActive.classList.remove(this.select.class.option.selected);
-		selectDefault.options[selectDefault.selectedIndex].removeAttribute('selected');
+		defaultSelect.options[defaultSelect.selectedIndex].removeAttribute('selected');
 
 		option.classList.add(this.select.class.option.selected);
 		btn.innerText = option.innerText;
-		selectDefaultActive.setAttribute('selected', 'selected');
+		defaultSelectActive.setAttribute('selected', 'selected');
 	}
 
 	selectedOption(select, option) {
@@ -73,14 +96,14 @@ class Select {
 
 		const parent = option.closest(`.${this.select.class.parent}`);
 		const btn = parent.querySelector(`.${this.select.class.btn}`);
-		const selectDefault = parent.querySelector('select');
-		const selectArr = [...selectDefault.options];
-		const selectDefaultActive = selectArr.find((defaultOption) => defaultOption.value === option.dataset.selectValue);
+		const defaultSelect = parent.querySelector('select');
+		const selectArr = [...defaultSelect.options];
+		const defaultSelectActive = selectArr.find((defaultOption) => defaultOption.value === option.dataset.selectValue);
 
 		if (select.classList.contains(this.select.class.select.multiple)) {
-			this.selectedMultipleOption(option, btn, selectArr, selectDefault, selectDefaultActive);
+			this.selectedMultipleOption(option, btn, selectArr, defaultSelect, defaultSelectActive);
 		} else {
-			this.selectedSingleOption(option, btn, parent, selectDefault, selectDefaultActive);
+			this.selectedSingleOption(option, btn, parent, defaultSelect, defaultSelectActive);
 		}
 	}
 
@@ -91,7 +114,7 @@ class Select {
 
 			const select = e.target.closest(`.${this.select.class.select.default}`);
 			const label = e.target.closest(`.${this.select.class.label}`);
-			const list = e.target.closest(`.${this.select.class.list}`);
+			const popup = e.target.closest(`.${this.select.class.popup}`);
 			const option = e.target.closest(`.${this.select.class.option.default}`);
 			const selectActive = document.querySelector(`.${this.select.class.select.default}.${this.select.class.select.show}`);
 			const btnActive = selectActive ? selectActive.querySelector(`.${this.select.class.btn}`) : undefined;
@@ -101,13 +124,49 @@ class Select {
 			} else if (this.select.show && btn && btn !== btnActive) {
 				this.closeSelect(selectActive);
 				this.openSelect(select);
-			} else if (this.select.show && !list && !label) {
+			} else if (this.select.show && !popup && !label) {
 				this.closeSelect(selectActive);
-			} else if (this.select.show && list && option) {
+			} else if (this.select.show && popup && option) {
 				this.selectedOption(select, e.target);
 				if (!select.classList.contains(this.select.class.select.multiple)) this.closeSelect(selectActive);
 			}
 		});
+	}
+
+	changeSearchInput(input, content, optionArr) {
+		const plug = content.querySelector(`.${this.select.class.plug.default}`);
+		const optionResult = optionArr.filter((option) => option.innerText.toUpperCase().includes(input.value.toUpperCase()));
+
+		const addActive = () => {
+			optionResult.forEach((option) => {
+				option.classList.add(this.select.class.option.active);
+				const group = option.closest(`.${this.select.class.group.default}`);
+				if (group) group.classList.add(this.select.class.group.active);
+			});
+		};
+
+		const clearActive = () => {
+			optionArr.forEach((option) => {
+				option.classList.remove(this.select.class.option.active);
+				const group = option.closest(`.${this.select.class.group.default}`);
+				if (group) group.classList.remove(this.select.class.group.active);
+			});
+		};
+
+		plug.classList.remove(this.select.class.plug.active);
+		clearActive();
+
+		if (input.value.length) {
+			input.classList.add(this.select.class.input.active);
+			addActive();
+
+			const activeArr = content.querySelectorAll(`.${this.select.class.option.active}`);
+			if (!activeArr.length) plug.classList.add(this.select.class.plug.active);
+		} else {
+			input.classList.remove(this.select.class.input.active);
+			plug.classList.remove(this.select.class.plug.active);
+			clearActive();
+		}
 	}
 
 	createOption(list, btn, defaultParent) {
@@ -142,48 +201,71 @@ class Select {
 		}
 	}
 
-	hasSelectedOption(defaultSelect, btn) {
-		const selected = defaultSelect.querySelector('option[selected]');
-		if (selected) return;
-
-		if (!defaultSelect.hasAttribute('multiple')) {
-			const options = defaultSelect.querySelectorAll('option');
-			options[0].setAttribute('selected', 'selected');
-		} else {
-			btn.innerText = `${this.select.multiple.text} 0`;
-		}
-	}
-
-	createListSelect(wrapper, btn, defaultParent) {
+	createListSelect(defaultParent, content, btn) {
 		const list = document.createElement('ul');
 		list.className = this.select.class.list;
-		wrapper.append(list);
+		content.append(list);
 
 		this.createOption(list, btn, defaultParent);
 	}
 
-	createGroupSelect(wrapper, btn, defaultSelect) {
+	createGroupSelect(defaultSelect, content, btn) {
 		const optgroup = defaultSelect.querySelectorAll('optgroup');
 
 		if (!optgroup.length) return;
 
 		optgroup.forEach((defaultGroup) => {
 			const group = document.createElement('div');
-			group.className = this.select.class.group;
-			wrapper.append(group);
+			group.className = this.select.class.group.default;
+			content.append(group);
 
 			const label = document.createElement('b');
 			label.className = this.select.class.label;
 			label.innerText = defaultGroup.getAttribute('label');
 			group.append(label);
 
-			this.createListSelect(group, btn, defaultGroup);
+			this.createListSelect(defaultGroup, group, btn);
 		});
 	}
 
-	hasMultipleSelect(defaultSelect, select) {
-		if (!defaultSelect.hasAttribute('multiple')) return;
-		select.classList.add(this.select.class.select.multiple);
+	createSearchSelect(defaultSelect, popup, content) {
+		if (!defaultSelect.hasAttribute('autocomplete', 'on')) return;
+
+		const input = document.createElement('input');
+		input.setAttribute('type', this.select.search.input.type);
+		input.setAttribute('placeholder', this.select.search.input.placeholder);
+		input.className = this.select.class.input.default;
+		popup.prepend(input);
+
+		const plug = document.createElement('span');
+		plug.classList.add(this.select.class.plug.default);
+		plug.innerText = this.select.search.plug.text;
+		content.append(plug);
+
+		const options = content.querySelectorAll(`.${this.select.class.option.default}`);
+		const optionArr = [...options];
+
+		input.addEventListener('input', (e) => this.changeSearchInput(e.target, content, optionArr));
+	}
+
+	setTypeSelect(defaultSelect, select) {
+		if (defaultSelect.hasAttribute('multiple')) {
+			select.classList.add(this.select.class.select.multiple);
+		} else {
+			select.classList.add(this.select.class.select.single);
+		}
+	}
+
+	hasSelectedOption(defaultSelect, btn) {
+		const selected = defaultSelect.querySelector('option[selected]');
+		if (selected) return;
+
+		if (defaultSelect.hasAttribute('multiple')) {
+			btn.innerText = `${this.select.multiple.text} 0`;
+		} else {
+			const options = defaultSelect.querySelectorAll('option');
+			options[0].setAttribute('selected', 'selected');
+		}
 	}
 
 	createSelect() {
@@ -199,18 +281,23 @@ class Select {
 			parent.append(select);
 
 			const btn = document.createElement('button');
-			btn.className = this.select.class.btn;
 			btn.setAttribute('type', 'button');
+			btn.className = this.select.class.btn;
 			select.append(btn);
 
-			const wrapper = document.createElement('div');
-			wrapper.className = this.select.class.wrapper;
-			select.append(wrapper);
+			const popup = document.createElement('div');
+			popup.className = this.select.class.popup;
+			select.append(popup);
+
+			const content = document.createElement('div');
+			content.className = this.select.class.content;
+			popup.append(content);
 
 			this.hasSelectedOption(defaultSelect, btn);
-			this.createListSelect(wrapper, btn, defaultSelect);
-			this.createGroupSelect(wrapper, btn, defaultSelect);
-			this.hasMultipleSelect(defaultSelect, select);
+			this.setTypeSelect(defaultSelect, select);
+			this.createListSelect(defaultSelect, content, btn);
+			this.createGroupSelect(defaultSelect, content, btn);
+			this.createSearchSelect(defaultSelect, popup, content);
 		});
 	}
 
